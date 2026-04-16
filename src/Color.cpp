8ce3f0c025c6127fdb1890a32b6d1077e047177c
laikatoolbox <laikatoolbox@outlook.com>
2026-04-16 08:49:38 -0600

    Init

---
+++ src/Color.cpp
@@ -0,0 +1,7 @@
+#include "Color.h"
+
+namespace LaikaShared {
+
+Color::Color() {}
+
+} // namespace LaikaShared
---
+++ src/Color.h
@@ -0,0 +1,13 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+namespace LaikaShared {
+
+class Color {
+public:
+    Color();
+};
+
+} // namespace LaikaShared
+
+#endif // COLOR_H
